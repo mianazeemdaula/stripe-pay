90fff8ac634e4195bb31755eca6dda1df7047c81
mianazeemdaula <mianazeemdaula@gmail.com>
2023-09-11 15:00:15 +0500

    s

--- app/Http/Controllers/Api/PaymentHooksController.php
+++ app/Http/Controllers/Api/PaymentHooksController.php
@@ -17,24 +17,24 @@ class PaymentHooksController extends Controller
 {
     
     function stripePayment(Request $event) {
-        // try {
+        try {
             Log::debug($event->all());
-        //     if($event->id && $event->type = 'payment_intent.succeeded') {
-        //         DB::beginTransaction();
-        //         $invoice = Invoice::where('invoice_id', $event->data['object']['metadata']['invoice_id'])->first();
-        //         if($invoice) {
-        //             $invoice->status = 'paid';
-        //             $invoice->response = $event->all();
-        //             $invoice->data = $event->data['object']['metadata'];
-        //             $invoice->amount_paid = $event->data['object']['amount_received'];
-        //             $invoice->save();
-        //             $invoice->user->updateBalance($invoice->amount_paid, 'Payment received for invoice #'.$invoice->invoice_id);
-        //         }
-        //         DB::commit();
-        //     }
-        // } catch (\Exeception $th) {
-        //     DB::rollBack();
-        //     Log::debug($th);
-        // }
+            if($event->id && $event->type = 'payment_intent.succeeded') {
+                DB::beginTransaction();
+                $invoice = Invoice::where('invoice_id', $event->data['object']['metadata']['invoice_id'])->first();
+                if($invoice) {
+                    $invoice->status = 'paid';
+                    $invoice->response = $event->all();
+                    $invoice->data = $event->data['object']['metadata'];
+                    $invoice->amount_paid = $event->data['object']['amount_received'];
+                    $invoice->save();
+                    $invoice->user->updateBalance($invoice->amount_paid, 'Payment received for invoice #'.$invoice->invoice_id);
+                }
+                DB::commit();
+            }
+        } catch (\Exeception $th) {
+            DB::rollBack();
+            Log::debug($th);
+        }
     }
 }
