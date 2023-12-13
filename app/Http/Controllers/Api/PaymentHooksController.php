fcef7caefe57d0c1ee18170d881716d7ccf6e0d4
mianazeemdaula <mianazeemdaula@gmail.com>
2023-12-13 14:00:40 +0500

    as

--- app/Http/Controllers/Api/PaymentHooksController.php
+++ app/Http/Controllers/Api/PaymentHooksController.php
@@ -24,6 +24,7 @@ class PaymentHooksController extends Controller
     public function stripePayment(Request $event) {
         try {
             Log::debug($event->headers->all());
+            Log::debug($event->all());
             if($event->id && $event->type == 'payment_intent.succeeded') {
                 if(isset($event->data['object']['metadata']['user_tag'])){
                     DB::beginTransaction();
