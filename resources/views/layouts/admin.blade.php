d303e9c4879840ba6b0427fe7f030df336ce21cc
mianazeemdaula <mianazeemdaula@gmail.com>
2023-12-14 18:03:42 +0500

    as

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -7,7 +7,7 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <meta name="csrf-token" content="{{ csrf_token() }}">
     @vite(['resources/css/app.css', 'resources/js/app.js'])
-    <title>Universal Games</title>
+    <title>Shop CodeCryptex</title>
     <style>
         body {
             font-family: 'Poppins-Regular', sans-serif;
